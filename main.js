8843c98eb8e2c60d2e5ebff2541a2d6a1015480c
Magdalena Lisicka <magdalisicka@yahoo.co.uk>
2020-05-06 08:39:43 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,9 @@
+//alert ('sie ma!');
+//console.log('Uwaga, Uwaga');
+//console.log('Hej hej');
+// camelCase do nazwy zmiennych ,  variables (tbc)
+const firstName ='Magda';
+const age='49';
+console.log(firstName);
+console.log(age);
+console.log(`Sie ma , nazwam sie ${firstName} i mam ${age} lat`)
