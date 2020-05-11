dc00eb3426a12a1c3b02e339087b260e97af3032
Magdalena Lisicka <magdalisicka@yahoo.co.uk>
2020-05-12 00:03:35 +0100

    after js changes

--- main.js
+++ main.js
@@ -1,12 +1,17 @@
 //alert ('sie ma!');
 //console.log('Uwaga, Uwaga');
 //console.log('Hej hej');
-// camelCase do nazwy zmiennych ,  variables (tbc)
+//camelCase do nazwy zmiennych ,  variables (tbc)
 //const firstName ='Magda';
 //const age='49';
 //console.log(firstName);
 //console.log(age);
 //console.log(`Sie ma , nazwam sie ${firstName} i mam ${age} lat`)
 
-const heading=document.querySelector ('main-header');
+const heading=document.querySelector('.main-heading--js');
+heading.innerHTML=`Learning HTML `
+
 console.log(heading);
+
+//const heading=document.querySelector('body');
+//console.log(heading);
