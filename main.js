6f586cb4ed89e5818643e9e4776b754e7a1bdd37
Magdalena Lisicka <magdalisicka@yahoo.co.uk>
2020-05-11 22:54:53 +0100

    changes after week4

--- main.js
+++ main.js
@@ -2,8 +2,11 @@
 //console.log('Uwaga, Uwaga');
 //console.log('Hej hej');
 // camelCase do nazwy zmiennych ,  variables (tbc)
-const firstName ='Magda';
-const age='49';
-console.log(firstName);
-console.log(age);
-console.log(`Sie ma , nazwam sie ${firstName} i mam ${age} lat`)
+//const firstName ='Magda';
+//const age='49';
+//console.log(firstName);
+//console.log(age);
+//console.log(`Sie ma , nazwam sie ${firstName} i mam ${age} lat`)
+
+const heading=document.querySelector ('main-header');
+console.log(heading);
