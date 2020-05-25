d16eded059893dd987436469942738ad066b08f7
Magdalena Lisicka <magdalisicka@yahoo.co.uk>
2020-05-25 14:48:00 +0100

    responsive website w6

--- main.js
+++ main.js
@@ -3,10 +3,17 @@ const button = document.querySelector('.action--js');
 
 console.log(button)
 
-button.addEventListener('click', () => {
+/*button.addEventListener('click', () => {
     const heading = document.querySelector(".main__heading--js");
     heading.innerHTML = `Witaj Drogi Odwiedzajacy, nazaywam sie Magda`;
-    console.log(heading.classList.contains('main__heading'));
-});
+    heading.classList.toggle('klasa-z-js');
+});*/
 
 
+const hamburger = document.querySelector.Selector('.hamburger--js');
+
+
+hamburger.addEventListener('click', () => {
+    const nav = document.querySelector('.navigation--js');
+    nav.classList.toggle('navigation--open');
+})
